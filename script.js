ec1b77109ee77c6252fb7ada3cac8d4d1d959405
sammy wlodawski <samwlodawski@gmail.com>
2024-12-08 20:02:29 -0500

    Adding conditional logic to set the min and max that the slider can go to and setting logic to compare the current value against a new value.

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ let priceGap = 500;
 const priceInputvalue = document.querySelectorAll(".price-input input");
 for( let i = 0; i < priceInputvalue.length; i++ ) {
     priceInputvalue[i].addEventListener("input", e => {
-        
+        let minp = parseInt(priceInputvalue[0].value);
+        let maxp = parseInt(priceInputvalue[1].value);
+        let diff = maxp - minp;
+
+        if (minp < 0) {
+            alert("minimum price cannot be less than 0");
+            priceInputvalue[0].value = 0;
+            minp = 0;
+        }
+        if(maxp < 10000) {
+            alert("maximum value cannot be greater than 10000");
+            priceInputvalue[1].value = 10000;
+            maxp = 10000;
+        }
     })
 }
