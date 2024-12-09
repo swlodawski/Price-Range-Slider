85706ec84735f65c5a3b73218a6fa35499580658
sammy wlodawski <samwlodawski@gmail.com>
2024-12-08 19:51:53 -0500

    Setting up global variables and beginning conditional logic to compare values on the number slider.

---
+++ script.js
@@ -0,0 +1,11 @@
+const rangevalue = document.querySelector(".slider-container .price-slider");
+const rangeInputvalue = document.querySelectorAll(".range-input input");
+
+let priceGap = 500;
+
+const priceInputvalue = document.querySelectorAll(".price-input input");
+for( let i = 0; i < priceInputvalue.length; i++ ) {
+    priceInputvalue[i].addEventListener("input", e => {
+        
+    })
+}
